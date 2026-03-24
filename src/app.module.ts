334f819e97ca0c9dfef0a46da9badada52dfa035
Fufa Wakgari <fufawakgari174@gmail.com>
2026-03-24 13:11:25 +0300

    generate business line module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { BusinessLineModule } from './business-lines/business-lines.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/appointment-booking'),
     UsersModule,
+    BusinessLineModule,
   ],
   controllers: [AppController],
   providers: [AppService],
